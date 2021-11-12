9aafa1c7ff76011cb8abc524ead6d2cd42d8443d
Arafat <arafat97alim@gmail.com>
2021-11-13 02:18:29 +0530

    added navbar class based component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Cart from "./Cart";
+import Navbar from "./Components/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Cart />
       </div>
     );
